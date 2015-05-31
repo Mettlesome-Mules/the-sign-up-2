ca492ec6db971bf86cc800bf6c227051d44aa9e7
jmunsch <james.a.munsch@gmail.com>
2015-05-31 00:17:34 -0500

    Added a GrabStuff factory. Copied and pasted the auth.service.js into it. Need to do some more reading on $resource as it is a higher level $http which would be used like $http.get(...) to make lower level RESTful calls to the server API endpoints.
       Changes to be committed:
      	modified:   client/app/profile/profile.controller.js
      	modified:   client/app/profile/profile.html
      	new file:   client/components/the-sign-up/grab-stuff.service.js
      	modified:   client/index.html

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('theSignUp2App')
-  .controller('ProfileCtrl', function ($scope, $http, $cookieStore, User, Auth) {
+  .controller('ProfileCtrl', function ($scope, $http, $cookieStore, User, Auth, GrabStuff) {
       $scope.errors = {};
       $scope.users = {};
       $scope.isPressed = false;
+      $scope.isMessagePressed = false;
+      $scope.isServicesPressed = false;
       $scope.currentUser = Auth.getCurrentUser();
       $scope.showPassword = function(e){
         if (!$scope.isPressed){
@@ -16,7 +18,21 @@ angular.module('theSignUp2App')
         console.log($scope.isPressed)
       };
 
-      $scope.getCurrentUser = function(){
+      $scope.showMessages = function(e){
+        if (!$scope.isMessagePressed){
+          $scope.isMessagePressed = true
+        } else {
+          $scope.isMessagePressed = false
+        }
+        console.log($scope.isMessagePressed)
+      };
 
+      $scope.showServices = function(e){
+        if (!$scope.isServicesPressed){
+          $scope.isServicesPressed = true
+        } else {
+          $scope.isServicesPressed = false
+        }
+        console.log($scope.isServicesPressed)
       };
   });
