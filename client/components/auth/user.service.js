5bcfd002ff23ed12275ecedd4a7a3f875d74d2c4
jmunsch <james.a.munsch@gmail.com>
2015-05-31 00:22:45 -0500

    Actually it looks like auth.service.js uses $http should we bother with $resource? as seen in user.service.js?

--- client/components/auth/user.service.js
+++ client/components/auth/user.service.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('theSignUp2App')
+  // ********************************//
+  // $resource is a high level $http
+  // it handles $http.get and $http.post
+  // but its more configurable
+  // ********************************//
   .factory('User', function ($resource) {
     return $resource('/api/users/:id/:controller', {
       id: '@_id'
