78a77509ad41d5101957355b513c4246fdd38a1b
jmunsch <james.a.munsch@gmail.com>
2015-06-05 13:52:14 -0500

    Fixed lastMessages on seed.js
     Changes to be committed:
    	modified:   client/app/account/profile/profile.controller.js
    	modified:   server/config/seed.js

--- server/config/seed.js
+++ server/config/seed.js
@@ -14,6 +14,7 @@ var Message = require('../api/message/message.model')
 // Called after users are created
 //*******************************//
 var setupMessages = function(){
+  console.log('creating messages')
   Message.find({}).remove(function(){
     Message.create({
         title:  'Hello BOB',
@@ -51,6 +52,7 @@ var setupMessages = function(){
           User.find({ 'name': 'Test2 User'}, function(err, user2){
             if (user1[0] && user2[0])
             {
+              console.log('creating chat history')
               Message.create(
                 {
                   title:  'Hello'+user2[0].name,
@@ -76,44 +78,9 @@ var setupMessages = function(){
                   comments: [{ body: 'this is a comment', date: Date() }],
                   date: { type: Date, default: Date.now() },
                   hidden: false
-                }, 
-                function(err)
-                {
-                  if(err){console.log(err)}
-                  console.log('users found')
-                  Message.find({ title: 'HelloTest2 User' }, function(err, msg1)
-                  {
-                    if (err){console.log(err)}
-                    Message.find({ body: 'Test User!? WHYYY!? Sincerly Test2 User' }, function(err, msg2)
-                    {
-                      if ( msg1[0] && msg2[0] )
-                      {
-                        user1.lastMessages = [{
-                          fromPicUrl: user2[0].profileInfo.profilePicUrl, 
-                          msgBody: msg2.body,
-                          timestamp: msg2.date,
-                          fromUserName: user2.name,
-                          fromUserId: msg2.fromUserId
-                        }] 
-                        user2.lastMessages = [{
-                          fromPicUrl: user1[0].profileInfo.profilePicUrl, 
-                          msgBody: msg1.body,
-                          timestamp: msg1.date,
-                          fromUserName: user1.name,
-                          fromUserId: msg1.fromUserId
-                        }]  
-                        user1[0].save()
-                        user2[0].save()
-                        console.log('messages populated')       
-                      } 
-                      else 
-                      {
-                        console.log(msg1, msg2)
-                      }
-                    })
-                  })
                 }
               )
+              setupJobs()
             }
           })
         })
@@ -122,10 +89,30 @@ var setupMessages = function(){
   })
 }
 
+var addTestFriends = function() {
+    User.find({ 'name': 'Test User'}, function(err, user1){
+    if (err){console.log(err)}
+    User.find({ 'name': 'Test2 User'}, function(err, user2){
+      user1[0].friends.push(user2[0]._id)
+      user2[0].friends.push(user1[0]._id)
+      user1[0].save(function (err) {
+        if (err) { console.log(err) }
+      })
+      user2[0].save(function (err) {
+        if (err) { console.log(err) }
+      })
+    })
+  })
+  console.log('finished populating users');
+  setupMessages()
+}
+
+
 //*******************************//
 // Called after users and messages are created
 //*******************************//
 var setupJobs = function(){
+  console.log('creating test jobs')
   Job.find({}).remove(function(){
     User.find({ 'name': 'Test User'}, function(err, user1){
       if (err){console.log(err)}
@@ -169,7 +156,10 @@ var setupJobs = function(){
             active: true
           }, function(err) {
             if(err){console.log(err)}
-            else{console.log('Jobs added')}
+            else{
+              console.log('Jobs added')
+              setupLastMessages()
+            }
           })
         }
       });
@@ -177,6 +167,60 @@ var setupJobs = function(){
   })
 }
 
+var setupLastMessages = function(){
+  console.log('creating lastMessages')
+  User.find({ email: 'test@test.com' }, function(err, user1)
+  {
+    if (err){console.log(err)}
+    User.find({ name: 'Test2 User' }, function(err, user2)
+    {
+      if(err){console.log(err)}
+      if (!user1[0] || !user2[0]){
+        console.log('seed.js: setupLastMessages: DID NOT FIND BOTH USERS')
+        return
+      }
+      console.log('seed.js: setupLastMessages: Users found')
+      Message.find({ fromUserId: user1[0]._id }, function(err, msg1)
+      {
+        if (err){console.log(err)}
+        Message.find({ fromUserId: user2[0]._id }, function(err, msg2)
+        {
+          if ( msg1[0] && msg2[0]) {
+            user1[0].lastMessages.push({
+              fromPicUrl: user2[0].profileInfo.profilePicUrl, 
+              msgBody: msg2[0].body,
+              timestamp: msg2[0].date,
+              fromUserName: user2[0].name,
+              fromUserId: msg2[0].fromUserId
+            })
+            user2[0].lastMessages.push({
+              fromPicUrl: user1[0].profileInfo.profilePicUrl, 
+              msgBody: msg1[0].body,
+              timestamp: msg1[0].date,
+              fromUserName: user1[0].name,
+              fromUserId: msg1[0].fromUserId
+            })
+            user1[0].save(function(err, suc){
+              if(err){
+                console.log('seed.js: setupLastMessages: Error on save.1',err, arguments)
+              }
+            })
+            user2[0].save(function(err, suc){
+              if(err){
+                console.log('seed.js: setupLastMessages: Error on save.2',err, arguments)
+              }
+            })
+            console.log('messages populated')       
+          } 
+          else 
+          {
+            console.log('MESSAGES NOT POPULATED',msg1, msg2)
+          }
+        })
+      })
+    })
+  })
+}
 
 Thing.find({}).remove(function() {
   Thing.create({
@@ -205,47 +249,35 @@ User.find({}).remove(function() {
   User.create({
     provider: 'local',
     name: 'Test User',
+    lastMessages: [],
     email: 'test@test.com',
     password: 'test',
     profileInfo: {profilePicUrl: './assets/images/grey_circle_user.png'}
   }, {
     provider: 'local',
+    lastMessages: [],
     name: 'Test2 User',
     email: 'test2@test2.com',
     password: 'test2',
     profileInfo: {profilePicUrl: './assets/images/grey_circle_user.png'}
   }, {
     provider: 'local',
+    lastMessages: [],
     role: 'admin',
     name: 'Admin',
     email: 'admin@admin.com',
     password: 'admin'
   }, {
     provider: 'local',
+    lastMessages: [],
     role: 'admin',
     name: 'mettle',
     email: 'mettlesome.mules.dev@gmail.com',
     password: 'neigh123'
   }, function() {
-      User.find({ 'name': 'Test User'}, function(err, user1){
-        if (err){console.log(err)}
-        User.find({ 'name': 'Test2 User'}, function(err, user2){
-          user1[0].friends.push(user2[0]._id)
-          user2[0].friends.push(user1[0]._id)
-          user1[0].save(function (err) {
-            if (err) { console.log(err) }
-          })
-          user2[0].save(function (err) {
-            if (err) { console.log(err) }
-          })
-        })
-      })
-      console.log('finished populating users');
-      setupMessages()
-      setupJobs()
+      addTestFriends()
     }
   );
 });
-
 
 
