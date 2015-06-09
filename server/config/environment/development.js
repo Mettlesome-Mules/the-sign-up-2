f11a5c48ac3e592749a7e7affbfd1518fa0c35a3
Garrett <gfitzgerald123@gmail.com>
2015-06-09 14:50:14 -0500

    set up remote db

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/thesignup2-dev'
+    uri: 'mongodb://dapperdingo:dapperpw@ds037812.mongolab.com:37812/legacy'
   },
 
   seedDB: true
