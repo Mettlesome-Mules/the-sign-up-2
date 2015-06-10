214bdfa03e7e60e83fc6cd0bf3089e2dc4a80372
Dan Corman <dan.corman@gmail.com>
2015-06-10 15:55:46 -0500

    Before API

--- client/app/maps/maps.controller.js
+++ client/app/maps/maps.controller.js
@@ -19,6 +19,38 @@ angular.module('theSignUp2App')
   uiGmapGoogleMapApi.then(function(maps) {
     $scope.map     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom };
     $scope.options = { scrollwheel: false };
-  });
+
+  var browserSupportFlag =  new Boolean();
+  var initialLocation;
+  var siberia = new google.maps.LatLng(60, 105);
+  var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+
+
+    if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      $scope.map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = siberia;
+    }
+    map.setCenter(initialLocation);
+  }
+})
 
 });
